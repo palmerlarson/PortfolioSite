d04b6854b9b9cb0c2cac11853d50f2a08e2009a6
Palmer Larson <palmerlarson@Palmers-MacBook-Pro.local>
2022-03-01 15:08:50 -0600

    initial

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+let x = document.getElementsByClassName("bar");
+
+const mobileNav = () => {
+    let checkBox = document.getElementById("myCheck");
+    let y = document.getElementById("burger");
+
+    if (checkBox.checked == true){
+        for (i=0; i < x.length; i++) {
+            x[i].style.display = "block";
+        }
+    } else {
+        for (i=0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+      }
+
+      if (y.style.display == "none") {
+        for (i=0; i < x.length; i++) {
+            x[i].style.display = "inline-block";
+        }
+      }
+}
+
+window.onresize = function(){
+    if (window.innerWidth > 1065) {
+        for (i=0; i < x.length; i++) {
+            x[i].style.display = "inline-block";
+        }
+    } else {
+        for (i=0; i < x.length; i++) {
+            x[i].style.display = "none";
+        }
+    }
+}
