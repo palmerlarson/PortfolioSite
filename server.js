6f3a1cf4e2126bfe57a749a34785495fc599b62e
Palmer Larson <palmerlarson@protonmail.com>
2022-07-01 18:17:42 -0500

    build

--- server.js
+++ server.js
@@ -10,16 +10,6 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
-app.post('/docs/palmerLarson2022.pdf', function(req, res, next) {
-  let file=fs.createReadStream('/dist/docs/palmerlarson2022.pdf');
-  var stat = fs.statSync('/dist/docs/palmerlarson2022.pdf');
-  res.setHeader('Content-Length', stat.size);
-  res.setHeader('Content-Type', 'application/pdf');
-  res.setHeader('Content-Disposition', 'attachment; filename=palmerLarson2022.pdf');
-  file.pipe(res);
-
-  stream.pipe(res);
-});
 
 
 
