2509af478ddca5786d046de6f05735de486530d9
Palmer Larson <palmerlarson@protonmail.com>
2022-07-01 17:52:59 -0500

    build

--- server.js
+++ server.js
@@ -10,5 +10,21 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
+app.post('/docs/palmerLarson2022', function(req, res, next) {
+  var stream = fs.readStream('/dist/docs/palmerLarson2022.pdf');
+  var filename = "palmerLarson2022.pdf"; 
+  // Be careful of special characters
+
+  filename = encodeURIComponent(filename);
+  // Ideally this should strip them
+
+  res.setHeader('Content-disposition', 'inline; filename="' + filename + '"');
+  res.setHeader('Content-type', 'application/pdf');
+
+  stream.pipe(res);
+});
+
+
+
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
