6eebdbd3718eeca3ae2df08d7ec33d50f3da0d07
Palmer Larson <palmerlarson@protonmail.com>
2022-07-01 17:42:55 -0500

    pdf work

--- server.js
+++ server.js
@@ -8,10 +8,7 @@ const port = process.env.PORT || 8080;
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/dist/index.html'));
-  res.download('/dist/docs/palmerlarson2022.pdf');
 });
-
-
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
