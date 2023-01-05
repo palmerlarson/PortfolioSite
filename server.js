61f93254d70f464863e9150e76679015684f068b
Palmer Larson <palmerlarson@protonmail.com>
2023-01-04 19:51:04 -0600

    fixed node extensions

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
-app.use(express.static("dist"));
 const port = process.env.PORT || 8080;
-
 // sendFile will go here
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
-});
+// app.get('/', function(req, res) {
+//   res.sendFile(path.join(__dirname, '/dist/index.html'));
+// });
 
-
-
+// app.use(express.static(__dirname + '/dist', {
+//   index:false,
+//   extensions: ['html']
+// }));
+app.use(express.static(__dirname + '/dist', {
+  extensions: ['html']
+}));
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
